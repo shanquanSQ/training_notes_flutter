50aced1389d91c56f2f94b4890858da13771f79d
shanquanSQ <shanquan93@hotmail.com>
2024-01-17 14:25:10 +0800

    buttons w dialog

--- lib/Screens/training_a.dart
+++ lib/Screens/training_a.dart
@@ -8,17 +8,26 @@ import 'package:trngprotocol_app/Components/daily_card.dart';
 class TrainingA extends StatelessWidget {
   // const TrainingA({super.key});
 
-  final List _exercises = ['Monday', 'Tuesday', 'Wednesday', 'Thursday'];
+  final List _exercises = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday'
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView.builder(
+          padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 50.0),
           itemCount: _exercises.length,
           itemBuilder: (context, index) {
             return DayCard(
               dayName: _exercises[index],
-              exerciseList: ['exercise 1', 'exercise 2'],
+              exerciseList: ['exercise 1', 'exercise 2'], // Will take from Database
             );
           }),
     );
