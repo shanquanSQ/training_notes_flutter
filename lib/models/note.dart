9f808d81a68c61ea3d18eb78893d037752608ab2
shanquanSQ <shanquan93@hotmail.com>
2024-01-17 12:33:34 +0800

    isar db initialised with notes model

---
+++ lib/models/note.dart
@@ -0,0 +1,11 @@
+import 'package:isar/isar.dart';
+
+part 'note.g.dart'; // This line is needed to generate file <filename>.g.dart
+// Then run: dart run build_runner build
+
+@collection
+// @Collection() // Add this line before the class
+class Note {
+  Id id = Isar.autoIncrement;
+  late String text;
+}
